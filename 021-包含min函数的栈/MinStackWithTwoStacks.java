98284a55f2d404fd7cf3841677bd9624e6d7c179
zelong <chen.zelong0814@gmail.com>
2018-06-06 22:20:13 +0200

    min stack with two stacks

--- 021-包含min函数的栈/MinStackWithTwoStacks.java
+++ 021-包含min函数的栈/MinStackWithTwoStacks.java
@@ -1,2 +1,28 @@
+import java.util.Stack;
+
 public class MinStackWithTwoStacks {
+
+	private Stack<Integer> dataStack = new Stack<>();
+	private Stack<Integer> minStack = new Stack<>();
+
+	public void push(int node) {
+		dataStack.push(node);
+		if (minStack.isEmpty() || minStack.peek() > node) {
+			minStack.push(node);
+		}
+	}
+
+	public void pop() {
+		if (dataStack.pop() == minStack.peek()) {
+			minStack.pop();
+		}
+	}
+
+	public int top() {
+		return dataStack.peek();
+	}
+
+	public int min() {
+		return minStack.peek();
+	}
 }
